88432a6227c86bb3bedf51242d6fcc48ee79ed79
terniumc.gruman <c.gruman@tessa.com.uy>
2022-03-21 17:27:00 -0300

    primer ejemplo comiteado

---
+++ ejemploModulos/iniciar_persona.py
@@ -0,0 +1,8 @@
+from persona import *
+
+
+gabo = Persona("Gabriel", "Rumani", "programador")
+
+gabo.hablar()
+
+gabo.saludar_trabajo(gabo.trabajo)
---
+++ ejemploModulos/uso.py
@@ -0,0 +1,18 @@
+import funciones_matematicas #form carpeta/funciones_matematicas 
+from atleta import *
+
+
+# funciones_matematicas.sumar(4,11)
+
+# funciones_matematicas.restar(5,2)
+
+# nuevo_ciclista = Ciclista ("bici")
+
+# nuevo_ciclista.get_estilo() 
+# nuevo_ciclista.set_estilo("boxeo")
+
+
+
+atleta_nuevo = TriAtleta("Mariposa", "500", "Urbano", "BMX", "Nicolas Perez", 31)
+
+print(f"{atleta_nuevo}")
